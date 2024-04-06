058dff1826af7471417b1cf016ef8cea090f74f5
Muhammad Abubakar <m3abubakar@gmail.com>
2024-04-06 18:16:07 +0500

    changing layout

--- components/Benefits.tsx
+++ components/Benefits.tsx
@@ -1,16 +1,19 @@
+'use client'
 import React from 'react'
 import Section from './Section'
 import Image from 'next/image'
 import { benefits } from '@/lib/constants'
-import { Heading } from './heading'
+
+import ClipPath from './ui/ClipPath'
+import Heading from './ui/Heading'
 
 const Benefits = () => {
   return (
-    <Section id="features">
+    <Section>
       <div className="container relative z-2">
         <Heading
-          className="md:max-w-md lg:max-w-2xl"
-          title="Chat Smarter, Not Harder with Brainwave"
+          className="lg:text-6xl md:text-3xl mx-auto font-normal text-xl mb-6 text-[#fff]"
+          title="Chat Smarter, Not Harder with AINA"
         />
 
         <div className="flex flex-wrap gap-10 mb-10">
@@ -22,9 +25,9 @@ const Benefits = () => {
               }}
               key={item.id}
             >
-              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
-                <h5 className="h5 mb-5">{item.title}</h5>
-                <p className="body-2 mb-6 text-n-3">{item.text}</p>
+              <div className="relative z-10 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                <h5 className="font-normal md:text-2xl text-xl mb-5">{item.title}</h5>
+                <p className="font-light text-gray-300 mb-6 text-n-3">{item.text}</p>
                 <div className="flex items-center mt-auto">
                   <Image
                     src={item.iconUrl}
@@ -35,14 +38,14 @@ const Benefits = () => {
                   <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
                     Explore more
                   </p>
-                  <Arrow />
+                  {/* <Arrow /> */}
                 </div>
               </div>
 
-              {item.light && <GradientLight />}
+              {item.light && <div className="absolute top-0 left-1/4 w-full aspect-square bg-radial-gradient from-[#28206C] to-[#28206C]/0 to-70% pointer-events-none" />}
 
               <div
-                className="absolute inset-0.5 bg-n-8"
+                className="absolute inset-0.5 bg-[#0e0c15] rounded-b-[30px] rounded-l-[30px] rounded-r-[25%]"
                 style={{ clipPath: "url(#benefits)" }}
               >
                 <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
@@ -58,7 +61,7 @@ const Benefits = () => {
                 </div>
               </div>
 
-              <ClipPath />
+              {/* <ClipPath /> */}
             </div>
           ))}
         </div>
