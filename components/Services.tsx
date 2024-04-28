b0c474b0194a496461ae941e8683ebf0cbaf1565
Muhammad Abubakar <m3abubakar@gmail.com>
2024-04-28 17:44:59 +0500

    changing layout of roadmap component

--- components/Services.tsx
+++ components/Services.tsx
@@ -122,6 +122,25 @@ const Services = () => {
 
           <BlockGradient/>
         </div>
+
+        <div className="hidden pt-4 mt-5 relative justify-center mb-[6.5rem] lg:flex">
+          <Image
+            src='/4-small.png'
+            className="relative z-1"
+            width={255}
+            height={255}
+            alt="Sphere"
+          />
+          <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+            <Image
+              src='/stars.svg'
+              className="w-full"
+              width={950}
+              height={400}
+              alt="Stars"
+            />
+          </div>
+        </div>
       </div>
     </Section>
   )
